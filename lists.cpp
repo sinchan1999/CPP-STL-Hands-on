6e9db4a78835846fa9669335b0c9e7908fab0e33
sinchan <sinchanpanda99@gmail.com>
2021-11-22 21:06:31 +0530

    Added Deque

--- lists.cpp
+++ lists.cpp
@@ -14,9 +14,12 @@ list<int> ls;
 	ls.push_front(5); // {5, 2, 4};
     
 	ls.emplace_front(); //{2, 4};
+    
 
 	// rest functions same as vector
 	// begin, end, rbegin, rend, clear, insert, size, swap
 
     return 0;
 }
+
+// In Lists we can push elements from the front whereas same can't be done in Vector.
